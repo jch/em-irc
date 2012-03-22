605abe30b5c77095cf495e500cfe548c6c982f7e
Jerry Cheung <jch@whatcodecraves.com>
2012-03-21 22:20:34 -0400

    add client commands. still a bunch more to go

--- lib/em-irc/commands.rb
+++ lib/em-irc/commands.rb
@@ -1,20 +1,18 @@
 module EventMachine
   module IRC
     # Client commands
-    # See [RFC 2812](http://tools.ietf.org/html/rfc2812)
+    # See {http://tools.ietf.org/html/rfc2812 RFC 2812}
     module Commands
-      # 3.1.1 Password message
-      #
-      # The PASS command is used to set a ’connection password’.  The
-      # optional password can and MUST be set before any attempt to register
-      # the connection is made.  Currently this requires that user send a
-      # PASS command before sending the NICK/USER combination.
+      # Set connection password
+      # @see http://tools.ietf.org/html/rfc2812#section-3.1.1 3.1.1 Password message
       def pass(password)
         send_data("PASS #{password}")
       end
 
+      # Set/get user nick
       # @return [String] nick if no param
       # @return nil otherwise
+      # @see http://tools.ietf.org/html/rfc2812#section-3.1.2 3.1.2 Nick Message
       def nick(nick = nil)
         if nick
           send_data("NICK #{nick}")
@@ -23,12 +21,92 @@ module EventMachine
         end
       end
 
+      # Set username, hostname, and realname
+      # @see http://tools.ietf.org/html/rfc2812#section-3.1.3 3.1.3 User Message
       def user(username, mode, realname)
         send_data("USER #{username} #{mode} * :#{realname}")
       end
 
-      def join(channel_name, channel_key = nil)
-        send_data("JOIN #{channel_name} #{channel_key}".strip)
+      # Gain operator privledges
+      # @see http://tools.ietf.org/html/rfc2812#section-3.1.4 3.1.4 Oper Message
+      def oper(name, password)
+        send_data("OPER #{name} #{password}")
+      end
+
+      # Set user mode
+      # @see http://tools.ietf.org/html/rfc2812#section-3.1.5 3.1.5 Mode Message
+      def mode(nickname, setting)
+        raise NotImplementedError.new
+      end
+
+      # Register a new service
+      # @see http://tools.ietf.org/html/rfc2812#section-3.1.6 3.1.6 Service Message
+      def service(nickname, reserved, distribution, type)
+      end
+
+      # Terminate connection
+      # @see http://tools.ietf.org/html/rfc2812#section-3.1.7 3.1.7 Quit
+      def quit(message = 'leaving')
+        send_data("QUIT :#{message}")
+      end
+
+      # Disconnect server links
+      # @see http://tools.ietf.org/html/rfc2812#section-3.1.8 3.1.8 Squit
+      def squit(server, message = "quiting")
+        raise NotImplementedError.new
+      end
+
+      # Join a channel
+      # @see http://tools.ietf.org/html/rfc2812#section-3.2.1 3.2.1 Join message
+      # @example
+      #   client.join("#general")
+      #   client.join("#general", "fubar")  # join #general with fubar key
+      #   client.join(['#general', 'fubar'], "#foo")  # join multiple channels
+      def join(*args)
+        raise ArgumentError.new("Not enough arguments") unless args.size > 0
+        channels, keys = [], []
+        args.map!  {|arg| arg.is_a?(Array) ? arg : [arg, '']}
+        args.sort! {|a,b| b[1].length <=> a[1].length}  # key channels first
+        args.each  {|arg|
+          channels << arg[0]
+          keys     << arg[1] if arg[1].length > 0
+        }
+        send_data("JOIN #{channels.join(',')} #{keys.join(',')}".strip)
+      end
+
+      # Part all channels
+      def part_all
+        join('0')
+      end
+
+      # Leave a channel
+      # @see http://tools.ietf.org/html/rfc2812#section-3.2.2 3.2.2 Part message
+      # @example
+      #   client.part('#general')
+      #   client.part('#general', '#foo')
+      #   client.part('#general', 'Bye all!')
+      #   client.part('#general', '#foo', 'Bye all!')
+      def part(*args)
+        raise ArgumentError.new("Not enough arguments") unless args.size > 0
+        message = %(# &).include?(args.last[0]) ? "Leaving..." : args.pop
+        send_data("PART #{args.join(',')} :#{message}")
+      end
+
+      # Set channel mode
+      # @todo name conflict with user MODE message
+      def channel_mode
+        raise NotImplementedError.new
+      end
+
+      # Set/get topic
+      # @param topic [Mixed] String, nil
+      #   non-blank string sets the topic
+      #   blank string unsets the topic
+      #   nil returns the current topic (default)
+      # @see http://tools.ietf.org/html/rfc2812#section-3.2.4 3.2.4 Topic message
+      def topic(channel, message = nil)
+        message = message.nil? ? "" : ":#{message}"
+        send_data("TOPIC #{channel} #{message}".strip)
       end
 
       def pong(servername)
@@ -42,9 +120,6 @@ module EventMachine
       end
       alias_method :message, :privmsg
 
-      def quit(message = 'leaving')
-        send_data("QUIT :#{message}")
-      end
     end
   end
 end
--- spec/lib/em-irc/commands_spec.rb
+++ spec/lib/em-irc/commands_spec.rb
@@ -1,5 +1,90 @@
 require 'spec_helper'
 
 describe EventMachine::IRC::Commands do
-  
+  subject {EventMachine::IRC::Client.new}
+
+  context "nick" do
+    it 'should set nick' do
+      subject.should_receive(:send_data).with("NICK jch")
+      subject.nick('jch')
+    end
+
+    it 'should get nick' do
+      subject.instance_eval {@nick = 'jch'}
+      subject.nick.should == 'jch'
+    end
+  end
+
+  context "join" do
+    it 'should join single channel' do
+      subject.should_receive(:send_data).with("JOIN #general")
+      subject.join("#general")
+    end
+
+    it 'should join multiple channels' do
+      subject.should_receive(:send_data).with("JOIN #general,#foo")
+      subject.join('#general', '#foo')
+    end
+
+    it 'should join channels with keys' do
+      subject.should_receive(:send_data).with("JOIN #foo,&bar fubar,bazly")
+      subject.join(['#foo', 'fubar'], ['&bar', 'bazly'])
+    end
+
+    it 'should join channels with and without keys' do
+      subject.should_receive(:send_data).with("JOIN #foo,#cat,&bar fubar,dog")
+      subject.join(['#foo', 'fubar'], '&bar', ["#cat", "dog"])
+    end
+
+    it 'should leave all currently joined channels' do
+      subject.should_receive(:join).with('0')
+      subject.join('0')
+    end
+  end
+
+  context "part_all" do
+    it 'should leave all currently joined channels' do
+      subject.should_receive(:join).with('0')
+      subject.part_all
+    end
+  end
+
+  context "part" do
+    it 'should part single' do
+      subject.should_receive(:send_data).with('PART #general :Leaving...')
+      subject.part('#general')
+    end
+
+    it 'should part single with custom message' do
+      subject.should_receive(:send_data).with('PART #general :Goodbye everybody!')
+      subject.part('#general', 'Goodbye everybody!')
+    end
+
+    it 'should part multiple' do
+      subject.should_receive(:send_data).with('PART #general,#foo :Leaving...')
+      subject.part('#general', '#foo')
+    end
+
+    it 'should part multiple with custom message' do
+      subject.should_receive(:send_data).with('PART #general,#foo :Goodbye everybody!')
+      subject.part('#general', '#foo', 'Goodbye everybody!')
+    end
+  end
+
+  context "topic" do
+    it 'should get topic' do
+      subject.should_receive(:send_data).with("TOPIC #foo")
+      subject.topic('#foo')
+    end
+
+    it 'should set topic' do
+      subject.should_receive(:send_data).with("TOPIC #foo :awesome channel")
+      subject.topic('#foo', "awesome channel")
+    end
+
+    it 'should unset topic' do
+      subject.should_receive(:send_data).with("TOPIC #foo :")
+      subject.topic('#foo', '')
+    end
+  end
 end
